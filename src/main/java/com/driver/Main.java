c818da6c7a30ba14a6d7c2defd188ef773c17a4c
Vaishnavi Sunil More <vaishnavimore9553@gmail.com>
2023-05-04 15:40:16 +0530

    Assignment Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        int a=8;int b=8;
+        RWOnly obj=new RWOnly(a,b);
+        System.out.println(a+" "+b);
+    }
   
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
+    private int a;
+    private int b;
+    public RWOnly(int a,int b){
+        a=this.a;
+        b=this.b;
+    }
     
 }
